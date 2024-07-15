53247dd1e690cf3108fefac833a061cb80e72ca7
slince-zero <1711257704@qq.com>
2024-07-15 14:52:17 +0800

    feat: 完成登录鉴权功能

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
 import { ArrowRight } from 'lucide-react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-const Navbar = () => {
-  const user = undefined
-  const isAdmin = false
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  const isAdmin = user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
@@ -30,7 +32,7 @@ const Navbar = () => {
 
                 {isAdmin ? (
                   <Link
-                    href='/api/auth/logout'
+                    href='/dashboard'
                     className={buttonVariants({
                       size: 'sm',
                       variant: 'ghost',
@@ -51,7 +53,7 @@ const Navbar = () => {
             ) : (
               <>
                 <Link
-                  href='/api/auth/logout'
+                  href='/api/auth/register'
                   className={buttonVariants({
                     size: 'sm',
                     variant: 'ghost',
