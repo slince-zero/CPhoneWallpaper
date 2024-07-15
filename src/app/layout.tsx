f1f8c0c53bb4505acbd5fa1d5cdd4a941a506146
slince-zero <1711257704@qq.com>
2024-07-15 18:44:57 +0800

    style: 细节调整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,10 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Navbar />
-        {children}
-        <Footer/>
+        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>{children}</div>
+          <Footer />
+        </main>
       </body>
     </html>
   )
