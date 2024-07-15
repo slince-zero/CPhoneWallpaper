81472930f5a3f9474a6978b7fd898c92067fdc38
slince-zero <1711257704@qq.com>
2024-07-15 16:43:16 +0800

    style: 细节修改

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,23 @@ import { buttonVariants } from '@/components/ui/button'
 import Reviews from '@/components/Reviews'
 export default function Home() {
   return (
-    <div className='bg-slate-50'>
+    <div className='bg-slate-50 grainy-light'>
       <section>
         <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52'>
-          <div className='ol-span-2 px-6 lg:px-0 lg:pt-4'>
+          <div className='col-span-2 px-6 lg:px-0 lg:pt-4'>
             <div className='relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start'>
               <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
                 <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
-                {/* 使用 Image 记得添加宽高 */}
                 <img
                   src='/snake-1.png'
-                  alt='snake'
+                  className='w-full'
                 />
               </div>
-
               <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
                 Your Image on a{' '}
                 <span className='bg-green-600 px-2 text-white'>Custom</span>{' '}
                 Phone Case
               </h1>
-
               <p className='mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap'>
                 Capture your favorite memories with your own,{' '}
                 <span className='font-semibold'>one-of-one</span> phone case.
@@ -79,6 +76,7 @@ export default function Home() {
                     alt='user image'
                   />
                 </div>
+
                 <div className='flex flex-col justify-between items-center sm:items-start'>
                   <div className='flex gap-0.5'>
                     <Star className='h-4 w-4 text-green-600 fill-green-600' />
@@ -95,6 +93,7 @@ export default function Home() {
               </div>
             </div>
           </div>
+
           <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
             <div className='relative md:max-w-xl'>
               <img
