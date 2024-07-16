557d93309304112af239286a18ab201db1aea6dd
slince-zero <1711257704@qq.com>
2024-07-16 16:07:05 +0800

    feat: 上传图片功能-第一阶段完成

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,12 @@
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams
+  return <p>{id}</p>
+}
+
+export default Page
